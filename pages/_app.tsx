9d220a1b37865f8b180fc40bca2011337e041a9a
foysalkazi11 <80021202+foysalkazi@users.noreply.github.com>
2021-10-27 11:54:14 +0600

    first commit with project setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Head>
+        <title>Your Monents</title>
+        <meta name="description" content="Share your moments with world" />
+        
+      </Head>
+
+    <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
